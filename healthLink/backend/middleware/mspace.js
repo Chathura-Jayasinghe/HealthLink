a12a49c3a4af30473401b04a33c5093605e205cd
kavindu-damsith-65 <thilakarathnakdb.21@uom.lk>
2023-09-30 02:44:53 +0530

    1

---
+++ healthLink/backend/middleware/mspace.js
@@ -0,0 +1,41 @@
+
+
+function cass(req, res, next){
+    const data = {
+        applicationId: process.env.APP_ID,
+        password: process.env.PASSWORD,
+        externalTrxId: "1234567890123456789012345678901223",
+        subscriberId: "tel:94701373491",
+        paymentInstrumentName: "Mobile Account",
+        accountId: process.env.ACCOUNTID,
+        amount: "5",
+        currency: "LKR"
+      };
+      
+      const axiosConfig = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+
+    axios.post('https://api.mspace.lk/caas/direct/debit', data, axiosConfig)
+    .then((response) => {
+      if(response.status===200){
+        next()
+      }else{
+        return res.status(250).json({
+            'message': "Something went wrong",
+        });
+      }
+    })
+    .catch((error) => {
+        return res.status(250).json({
+            'message': "Error!!",
+        });
+    });
+}
+
+module.exports = {
+    cass: cass
+}
