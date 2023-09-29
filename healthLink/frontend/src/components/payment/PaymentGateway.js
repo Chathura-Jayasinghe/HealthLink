56aee8a947b7feff1dbb7bf65ba3a6d7c87a9d9c
Malitha Prabhashana <110366097+MalithaPrabhashana@users.noreply.github.com>
2023-09-30 01:48:56 +0530

    Payment Gateway

--- healthLink/frontend/src/components/payment/PaymentGateway.js
+++ healthLink/frontend/src/components/payment/PaymentGateway.js
@@ -1,73 +1,113 @@
 import React from 'react';
 import './PaymentGateway.css';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
 
 export default function PaymentGateway() {
-    
     return (
         <div>
-            <button type="button" class="btn btn-primary gateway-launch" data-toggle="modal" data-target="#staticBackdrop"> <i class="fa fa-rocket"></i> Pay Now
-</button>
+            <button type="button" class="btn btn-primary gateway-launch" data-toggle="modal" data-target="#staticBackdrop"> <i class="fa fa-rocket"></i> Pay Now</button>
 
-        <div className="modal fade payment-body" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="payment-modal-body">
-                        <div className="text-right">
-                            <i className="fa fa-close close" data-dismiss="modal"></i>
-                        </div>
-                        <div className="tabs mt-3">
-                            <ul className="nav nav-tabs" id="myTab" role="tablist">
-                                <li className="nav-item" role="presentation">
-                                    <a className="nav-link active" id="visa-tab" data-toggle="tab" href="#visa" role="tab" aria-controls="visa" aria-selected="true">
-                                        <img src="https://i.imgur.com/sB4jftM.png" width="80" alt="Visa" />
-                                    </a>
-                                </li>
-                                <li className="nav-item" role="presentation">
-                                    <a className="nav-link" id="paypal-tab" data-toggle="tab" href="#paypal" role="tab" aria-controls="paypal" aria-selected="false">
-                                        <img src="https://i.imgur.com/yK7EDD1.png" width="80" alt="Paypal" />
-                                    </a>
-                                </li>
-                            </ul>
-                            <div className="tab-content" id="myTabContent">
-                                <div className="tab-pane fade show active" id="visa" role="tabpanel" aria-labelledby="visa-tab">
-                                    <div className="mt-4 mx-4">
-                                        <div className="text-center">
-                                            <h5>Credit card</h5>
-                                        </div>
-                                        <div className="form mt-3">
-                                            <div className="inputbox">
-                                                <input type="text" name="name" className="form-control" required="required" />
-                                                <span>Cardholder Name</span>
+            <div className="modal fade payment-body" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+               
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-body">
+
+                            <div className="text-right">
+                                <i className="fa fa-close close" data-dismiss="modal"></i>
+                            </div>
+                            <div className="tabs mt-3">
+                                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                                    <li className="nav-item" role="presentation">
+                                        <a className="nav-link active"  id="mspace-tab" data-toggle="tab" href="#mspace" role="tab" aria-controls="mspace" aria-selected="true">
+                                            <img src="https://user.mspace.lk/registration-api/api/v3/public/mobitel/icons/company-logo-white.png" width="80" alt="mspace" />
+                                        </a>
+                                    </li>
+                                    <li className="nav-item" role="presentation">
+                                        <a className="nav-link" id="visa-tab" data-toggle="tab" href="#visa" role="tab" aria-controls="visa" aria-selected="false">
+                                            <img src="https://i.imgur.com/sB4jftM.png" width="80" alt="Visa" />
+                                        </a>
+                                    </li>
+                                </ul>
+                                <div className="tab-content" id="myTabContent">
+                                    <div className="tab-pane fade show active" id="mspace" role="tabpanel" aria-labelledby="mspace-tab">
+                                        <div className="px-5 mt-3">
+                                            <div className="text-center">
+                                                <h5>Caas Payment</h5>
                                             </div>
-                                            <div className="inputbox">
-                                                <input type="text" name="name" min="1" max="999" className="form-control" required="required" />
-                                                <span>Card Number</span>
-                                                <i className="fa fa-eye"></i>
+                                            <div className="inputbox mt-4">
+                                                <TextField
+                                                    label="Your Mobitel Number"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    required
+                                                    defaultValue="0704934655"
+                                                />
                                             </div>
-                                            <div className="d-flex flex-row">
-                                                <div className="inputbox">
-                                                    <input type="text" name="name" min="1" max="999" className="form-control" required="required" />
-                                                    <span>Expiration Date</span>
-                                                </div>
-                                                <div className="inputbox">
-                                                    <input type="text" name="name" min="1" max="999" className="form-control" required="required" />
-                                                    <span>CVV</span>
-                                                </div>
-                                            </div>
-                                            <div className="px-5 pay">
-                                                <button className="btn btn-success btn-block">Add card</button>
+                                            <div className="pay px-5 mt-4">
+                                                <button className="btn btn-success btn-block">Pay Now</button>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="tab-pane fade" id="paypal" role="tabpanel" aria-labelledby="paypal-tab">
-                                    <div className="px-5 mt-5">
-                                        <div className="inputbox">
-                                            <input type="text" name="name" className="form-control" required="required" />
-                                            <span>Paypal Email Address</span>
-                                        </div>
-                                        <div className="pay px-5">
-                                            <button className="btn btn-primary btn-block">Add paypal</button>
+                                    <div className="tab-pane fade" id="visa" role="tabpanel" aria-labelledby="visa-tab">
+                                        <div className="mt-4 mx-4">
+                                            <div className="text-center">
+                                                <h5>Credit card</h5>
+                                            </div>
+                                            <div className="form mt-3">
+                                                <div className="inputbox">
+                                                    <TextField
+                                                        label="Cardholder Name"
+                                                        variant="outlined"
+                                                        fullWidth
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="inputbox mt-3">
+                                                    <TextField
+                                                        label="Card Number"
+                                                        variant="outlined"
+                                                        fullWidth
+                                                        required
+                                                        inputProps={{ min: 1, max: 999 }} // Use inputProps for min and max
+                                                        InputProps={{
+                                                            endAdornment: (
+                                                                <InputAdornment position="end">
+                                                                    <IconButton>
+                                                                        <Visibility /> {/* You can toggle visibility here */}
+                                                                    </IconButton>
+                                                                </InputAdornment>
+                                                            ),
+                                                        }}
+                                                    />
+                                                </div>
+                                                <div className="d-flex flex-row mt-3">
+                                                    <div style={{marginRight: '10px'}}>
+                                                        <TextField
+                                                            label="Expiration Date"
+                                                            variant="outlined"
+                                                            fullWidth
+                                                            required
+                                                            inputProps={{ min: 1, max: 999 }} // Use inputProps for min and max
+                                                        />
+                                                    </div>
+                                                    <div className="inputbox">
+                                                        <TextField
+                                                            label="CVV"
+                                                            variant="outlined"
+                                                            fullWidth
+                                                            required
+                                                            inputProps={{ min: 1, max: 999 }} // Use inputProps for min and max
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div className="px-5 pay mt-4">
+                                                    <button className="btn btn-primary btn-block">Pay Now</button>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -76,7 +116,6 @@ export default function PaymentGateway() {
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 }
